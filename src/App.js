d4ea5428cf615889eceb3009982c9eb85c40db07
surya <suryajoshi617@gmail.com>
2024-11-17 19:34:38 +0530

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import CategoryFilter from "./components/CategoryFilter";
+import Navbar from "./components/Navbar";
+import ProductCard from "./components/ProductCard";
+import ProductDetail from "./components/ProductDetail";
+import ProductList from "./components/ProductList";
+import SearchBar from "./components/SearchBar";
+import Sort from "./components/Sort";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/product/:id" element={<ProductDetail/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
